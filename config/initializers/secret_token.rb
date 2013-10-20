6481e3e3d25542f0e56d1e9af564d659a338bcf3
Sunil Rao <sunil2rao@gmail.com>
2013-10-19 21:23:16 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mancan::Application.config.secret_token = '421567697026a0e5718206fc90284c835deeba0dd032eb3f2dd5a5922c84d7dc3604ff8ce237a25872a2c558659c013af967e11f6a062b4190afdbaf689f4394'
